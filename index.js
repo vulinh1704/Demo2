12884dde8d40cf301889b26a685303939f09c771
vulinh1704 <truongdangvulinh1704@gmail.com>
2023-03-21 18:16:56 +0700

    third

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
-const router = require("./src/controller/router");
-const error = require("./src/controller/handle/notFoundRouter");
+// const router = require("./src/controller/router");
+// const error = require("./src/controller/handle/notFoundRouter");
 const typeFile = {
     'jpg': 'images/jpg',
     'png': 'images/png',
@@ -15,25 +15,27 @@ const typeFile = {
 }
 
 let server = http.createServer((req, res) => {
-    let pathname = req.url;
-    let matchPath = /\.js|\.css|\.png|\.jpg|\.ttf|\.woff|\.woff2|\.eot/
-    if (matchPath.test(pathname)) {
-        let contentType = typeFile[pathname.split('.')[1]];
-        console.log(22,pathname.split('.'))
-        res.writeHead(200, {'Content-Type' : contentType})
-        fs.createReadStream(__dirname + pathname).pipe(res);
-    } else {
-        const arrPath = pathname.split('/');
-        const trimPath = arrPath[arrPath.length - 1];
-        console.log(trimPath)
-        let chosenHandle;
-        if (typeof router[trimPath] === 'undefined') {
-            chosenHandle = error.showNotFound;
-        } else {
-            chosenHandle = router[trimPath];
-        }
-        chosenHandle(req, res);
-    }
+    // let pathname = req.url;
+    // let matchPath = /\.js|\.css|\.png|\.jpg|\.ttf|\.woff|\.woff2|\.eot/
+    // if (matchPath.test(pathname)) {
+    //     let contentType = typeFile[pathname.split('.')[1]];
+    //     console.log(22,pathname.split('.'))
+    //     res.writeHead(200, {'Content-Type' : contentType})
+    //     fs.createReadStream(__dirname + pathname).pipe(res);
+    // } else {
+    //     const arrPath = pathname.split('/');
+    //     const trimPath = arrPath[arrPath.length - 1];
+    //     console.log(trimPath)
+    //     let chosenHandle;
+    //     if (typeof router[trimPath] === 'undefined') {
+    //         chosenHandle = error.showNotFound;
+    //     } else {
+    //         chosenHandle = router[trimPath];
+    //     }
+    //     chosenHandle(req, res);
+    // }
+    res.write('Ling');
+    res.end()
 });
 
 server.listen(8080)
