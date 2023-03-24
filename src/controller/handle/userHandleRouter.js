48f17dacbabd9f0dad1defbb9b691fb3b7955330
vulinh1704 <truongdangvulinh1704@gmail.com>
2023-03-24 15:02:04 +0700

    nofication success

--- src/controller/handle/userHandleRouter.js
+++ src/controller/handle/userHandleRouter.js
@@ -27,8 +27,28 @@ class ProductHandle {
         req.on('end', () => {
             let user = qs.parse(data);
             userService.addUser(user).then(() => {
-                res.writeHead(301, {location: '/'});
-                res.end();
+                fs.readFile(__dirname + '/views/index.html', 'utf-8', async (err, indexHtml) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        res.writeHead(200, 'text/html');
+                        indexHtml = indexHtml.replace('<!--{NofiSuccess}-->', '<div class="row" id="nofi">\n' +
+                            '        <div class="col-md-4 col-lg-4" style="margin-top: 13%; margin-left: 80%">\n' +
+                            '            <div class="panel panel-default">\n' +
+                            '                <div class="panel-body">\n' +
+                            '                    <div class="alert alert-success">\n' +
+                            '                        <strong class="default"><i class="fa-solid fa-check"></i> Register success</strong>\n' +
+                            '                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>\n' +
+                            '                    </div>\n' +
+                            '                </div>\n' +
+                            '            </div>\n' +
+                            '        </div>\n' +
+                            '    </div>')
+                        indexHtml = indexHtml.replace('//call showAlert' , 'showAlert()')
+                        res.write(indexHtml);
+                        res.end();
+                    }
+                })
             })
 
         })
